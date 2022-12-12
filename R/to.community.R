9ae2a29f6193e7ae047ff770ea27b054fb57964b
anttonalberdi <anttonalberdi@gmail.com>
2022-12-12 14:56:28 +0100

    to.community function

--- R/to.community.R
+++ R/to.community.R
@@ -26,8 +26,8 @@ to.community <- function(GIFT_table,abundance_table,GIFT_db){
       cat("\tAs no relative abundance information was provided distillR\n")
       cat("will generate a single community with evenly weighed genomes\n")
       abundance_table <- rep(1/nrow(GIFT_table),nrow(GIFT_table))
-      names(abundance_table) <- rownames(abundance_table)
       abundance_table <- t(t(abundance_table))
+      rownames(abundance_table) <- rownames(GIFT_table)
       colnames(abundance_table) <- "Community"
 
       #Declare single community
@@ -37,18 +37,14 @@ to.community <- function(GIFT_table,abundance_table,GIFT_db){
       communities <- colnames(abundance_table)
     }
 
+  GIFT_community_table <- c()
+  m=0
+  for(community in communities){
+    m=m+1
+    community_GIFT <- colSums(sweep(GIFT_table,1,abundance_table[,m], FUN="*"))
+    GIFT_community_table <- rbind(GIFT_community_table,community_GIFT)
+  }
+  rownames(GIFT_community_table) <- communities
 
-  community_table <- t(as.data.frame(t(functions_table)) %>%
-    rownames_to_column('Code_function') %>%
-    left_join(GIFT_db[,c('Code_function', 'Type')], by = 'Code_function') %>%
-    #Calculate Biosynthesis and Degradation MCIs
-    group_by(Type) %>%
-    summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
-    #Calculate overall MCI
-    ungroup %>%
-    summarise(Type = c(Type, 'Overall'), across(where(is.numeric), ~ c(., mean(.)))) %>%
-    arrange(factor(Type, levels = unique(GIFT_db$Type))) %>%
-    column_to_rownames('Type'))
-
-  return(community_table)
+  return(GIFT_community_table)
 }
