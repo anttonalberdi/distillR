b41548f6c1e2f5801c457567e38a7d6c8ada1291
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-05-26 13:27:02 +0200

    Silence test

--- tests/testthat/test-distillq.R
+++ tests/testthat/test-distillq.R
@@ -1,10 +1,10 @@
-test_that("multiplication works", {
-  # actual <- distillq(
-  #   gene_count_table = distillR::gene_expression,
-  #   annotation_table = distillR::gene_annotations,
-  #   GIFT_db = distillR::GIFT_db,
-  #   genecol = 1,
-  #   genomecol = 2,
-  #   annotcol = c(9,10,19)
-  # )
-})
+# test_that("multiplication works", {
+#   actual <- distillq(
+#     gene_count_table = distillR::gene_expression,
+#     annotation_table = distillR::gene_annotations,
+#     GIFT_db = distillR::GIFT_db,
+#     genecol = 1,
+#     genomecol = 2,
+#     annotcol = c(9,10,19)
+#   )
+# })
