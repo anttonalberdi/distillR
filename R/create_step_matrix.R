1e09339e8f5905530d91c21675a400b587d3620b
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-05-26 14:09:52 +0200

    Lint create_step_matrix

--- R/create_step_matrix.R
+++ R/create_step_matrix.R
@@ -1,7 +1,10 @@
-#' Creates a matrix that specifies the hierarchical structure of the definition of a metabolic pathway/module
+#' Creates a matrix that specifies the hierarchical structure of the definition
+#' of a metabolic pathway/module
 #'
-#' @param def_decomp Vector of decomposed units of a definition string of a metabolic pathway/module (produced by decompose_definition())
-#' @param def_level Vector of the hierarchical level of decomposed units of a definition string of a metabolic pathway/module (produced by set_levels())
+#' @param def_decomp Vector of decomposed units of a definition string of a
+#' metabolic pathway/module (produced by decompose_definition())
+#' @param def_level Vector of the hierarchical level of decomposed units of a
+#' definition string of a metabolic pathway/module (produced by set_levels())
 #' @return A decomposed hierarchy matrix
 #' @examples
 #' create_step_matrix(definition_decomposed, definition_levels)
@@ -12,16 +15,21 @@ create_step_matrix <- function(def_decomp, def_level) {
   def_table <- as.data.frame(cbind(def_decomp, def_level))
 
   # Create level 0 structure
-  L0_splits <- rownames(def_table[((def_decomp == " ") | (def_decomp == "+") | (def_decomp == ",")) & (def_level == 0), ])
-  L0_group <- c()
+  level0_splits <- rownames(
+    def_table[
+      ((def_decomp == " ") | (def_decomp == "+") | (def_decomp == ",")) &
+      (def_level == 0),
+    ]
+  )
+  level0_group <- c()
   group <- 1
-  for (i in c(1:length(def_decomp))) {
-    if (i %in% L0_splits) {
+  for (i in seq_along(def_decomp)) {
+    if (i %in% level0_splits) {
       group <- group + 1
     }
-    L0_group <- c(L0_group, group)
+    level0_group <- c(level0_group, group)
   }
-  def_table[, "L0_group"] <- L0_group
+  def_table[, "L0_group"] <- level0_group
 
   # If more hierarchical levels exist, iterate across levels.
   max_level <- max(def_level)
@@ -29,10 +37,14 @@ create_step_matrix <- function(def_decomp, def_level) {
     levels <- c(1:max_level)
     # Next levels
     for (level in levels) {
-      splits <- rownames(def_table[((def_decomp == " ") | (def_decomp == "+") | (def_decomp == ",")) & (def_level <= level), ])
+      splits <-
+        rownames(def_table[
+          ((def_decomp == " ") | (def_decomp == "+") | (def_decomp == ",")) &
+          (def_level <= level),
+        ])
       level_group <- c()
       group <- 1
-      for (i in c(1:length(def_decomp))) {
+      for (i in seq_along(def_decomp)) {
         if (i %in% splits) {
           group <- group + 1
         }
@@ -48,7 +60,9 @@ create_step_matrix <- function(def_decomp, def_level) {
   }
 
   # Polish matrix
-  def_table[(def_table$def_decomp == "(") | (def_table$def_decomp == ")"), ] <- NA
+  def_table[
+    (def_table$def_decomp == "(") | (def_table$def_decomp == ")"),
+  ] <- NA
 
   # Print matrix
   return(def_table)
