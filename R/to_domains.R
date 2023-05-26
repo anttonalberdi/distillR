7ebdb6546b3abc379c90bf14ac3d494ce1533d14
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-05-26 15:45:55 +0200

    Lint to_domains and renaming

--- R/to_domains.R
+++ R/to_domains.R
@@ -1,34 +1,38 @@
 #' Aggregates function-level GIFTs into the domain level
 #'
 #' @param functions_table Function-level GIFT table
-#' @param GIFT_db Table containing definitions and metadata of GIFTs (default: database provided by distillR)
+#' @param gift_db Table containing definitions and metadata of GIFTs (default:
+#' database provided by distillR)
 #' @import tidyverse
 #' @import dplyr
 #' @return A table with Biosynthesis, Degradation and Overall MCI values
 #' @examples
-#' to.domains(functions_table, GIFT_db)
+#' to.domains(functions_table, gift_db)
 #' @export
 
 
-to.domains <- function(functions_table, GIFT_db) {
+to_domains <- function(functions_table, gift_db) {
   # Convert tables into data frames
   if (!missing(functions_table)) {
     functions_table <- as.data.frame(functions_table)
   }
-  if (!missing(GIFT_db)) {
-    GIFT_db <- as.data.frame(GIFT_db)
+  if (!missing(gift_db)) {
+    gift_db <- as.data.frame(gift_db)
   }
 
   domain_table <- t(as.data.frame(t(functions_table)) %>%
     rownames_to_column("Code_function") %>%
-    left_join(GIFT_db[, c("Code_function", "Domain")], by = "Code_function") %>%
+    left_join(gift_db[, c("Code_function", "Domain")], by = "Code_function") %>%
     # Calculate Biosynthesis and Degradation MCIs
     group_by(Domain) %>%
     reframe(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
     # Calculate overall MCI
     ungroup() %>%
-    reframe(Domain = c(Domain, "Overall"), across(where(is.numeric), ~ c(., mean(.)))) %>%
-    arrange(factor(Domain, levels = unique(GIFT_db$Domain))) %>%
+    reframe(
+      Domain = c(Domain, "Overall"),
+      across(where(is.numeric), ~ c(., mean(.)))
+    ) %>%
+    arrange(factor(Domain, levels = unique(gift_db$Domain))) %>%
     column_to_rownames("Domain"))
 
   return(domain_table)
--- tests/testthat/test-to_domains.R
+++ tests/testthat/test-to_domains.R
@@ -1,6 +1,6 @@
-test_that("multiplication works", {
+test_that("Test to_domains", {
   functions <- read_rds(test_path("fixtures", "gift_functions.rds"))
-  actual <- to.domains(functions, distillR::GIFT_db)
+  actual <- to_domains(functions, distillR::GIFT_db)
   # write_rds(actual, test_path("fixtures", "gift_domains.rds"))
   expected <- read_rds(test_path("fixtures", "gift_domains.rds"))
   expect_equal(actual, expected)
