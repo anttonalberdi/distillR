99fa799ca5d87570bfa469da08d0ad399878a48e
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-05-26 15:52:36 +0200

    Lint to_functions and renaming

--- R/to_functions.R
+++ R/to_functions.R
@@ -1,30 +1,34 @@
 #' Aggregates element-level GIFTs into the function level
 #'
 #' @param elements_table Element-level GIFT table
-#' @param GIFT_db Table containing definitions and metadata of GIFTs (default: database provided by distillR)
+#' @param gift_db Table containing definitions and metadata of GIFTs (default:
+#' database provided by distillR)
 #' @import tidyverse
 #' @import dplyr
 #' @return A GIFT table aggregated at the function level
 #' @examples
-#' to.functions(elements_table, GIFT_db)
+#' to.functions(elements_table, gift_db)
 #' @export
 
 
-to.functions <- function(elements_table, GIFT_db) {
+to_functions <- function(elements_table, gift_db) {
   # Convert tables into data frames
   if (!missing(elements_table)) {
     elements_table <- as.data.frame(elements_table)
   }
-  if (!missing(GIFT_db)) {
-    GIFT_db <- as.data.frame(GIFT_db)
+  if (!missing(gift_db)) {
+    gift_db <- as.data.frame(gift_db)
   }
 
   functions_table <- t(as.data.frame(t(elements_table)) %>%
     rownames_to_column("Code_element") %>%
-    left_join(GIFT_db[, c("Code_element", "Code_function")], by = "Code_element") %>%
+    left_join(
+      gift_db[, c("Code_element", "Code_function")],
+      by = "Code_element"
+    ) %>%
     group_by(Code_function) %>%
     summarise(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
-    arrange(factor(Code_function, levels = unique(GIFT_db$Code_function))) %>%
+    arrange(factor(Code_function, levels = unique(gift_db$Code_function))) %>%
     column_to_rownames("Code_function"))
 
   return(functions_table)
--- tests/testthat/test-to_functions.R
+++ tests/testthat/test-to_functions.R
@@ -1,6 +1,6 @@
-test_that("multiplication works", {
+test_that("Test to_functions", {
   elements <- read_rds(test_path("fixtures", "gift_elements.rds"))
-  actual <- to.functions(elements, distillR::GIFT_db)
+  actual <- to_functions(elements, distillR::GIFT_db)
   # write_rds(actual, test_path("fixtures", "gift_functions.rds"))
   expected <- read_rds(test_path("fixtures", "gift_functions.rds"))
   expect_equal(actual, expected)
