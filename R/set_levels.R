04e32fa6e5916fb81b8394819c576ef234cee169
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-05-26 16:52:41 +0200

    Simplify

--- R/set_levels.R
+++ R/set_levels.R
@@ -1,25 +1,26 @@
 #' Calculates the hierarchical levels of the definition of a metabolic
 #' pathway/module
 #'
-#' @param def_decomp A decomposed definition string of a given metabolic
-#' function (produced by decompose_definition())
+#' @param definition_decomposed A decomposed definition string of a given
+#' metabolic function (produced by decompose_definition())
 #' @return A vector of hierarchical levels per character in string
 #' @examples
 #' set_levels(definition_decomposed)
 #' @export
 
-set_levels <- function(def_decomp) {
-  def_level <- c()
+set_levels <- function(definition_decomposed) {
+  definition_levels <- list()
   level <- 0
-  for (i in seq_along(def_decomp)) {
-    chr <- def_decomp[i]
+  for (i in seq_along(definition_decomposed)) {
+    chr <- definition_decomposed[i]
     if (chr == "(") {
       level <- level + 1
     }
     if (chr == ")") {
       level <- level - 1
     }
-    def_level <- c(def_level, level)
+    definition_levels[[i]] <- level
   }
-  return(def_level)
+
+  return(definition_levels %>% as.integer())
 }
