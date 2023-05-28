ad3cb558a678e8762fda70503bc26ccffb8e7484
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-05-29 00:14:16 +0200

    Untangle to_domains

--- R/to_domains.R
+++ R/to_domains.R
@@ -12,21 +12,12 @@
 
 
 to_domains <- function(functions_table, gift_db) {
-  # Convert tables into data frames
-  if (!missing(functions_table)) {
-    functions_table <- as.data.frame(functions_table)
-  }
-  if (!missing(gift_db)) {
-    gift_db <- as.data.frame(gift_db)
-  }
 
   domain_table <- t(as.data.frame(t(functions_table)) %>%
     rownames_to_column("Code_function") %>%
     left_join(gift_db[, c("Code_function", "Domain")], by = "Code_function") %>%
-    # Calculate Biosynthesis and Degradation MCIs
     group_by(Domain) %>%  # nolint
     reframe(across(where(is.numeric), ~ mean(.x, na.rm = TRUE))) %>%
-    # Calculate overall MCI
     ungroup() %>%
     reframe(
       Domain = c(Domain, "Overall"),
