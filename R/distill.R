7f420c41866dd3389f6170553a7b24741bd5e3ec
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-06-06 11:54:12 +0200

    Refactor distill

--- R/distill.R
+++ R/distill.R
@@ -21,7 +21,8 @@
 #' @export
 
 distill <- function(
-    annotation_table, giftdb,
+    annotation_table,
+    giftdb,
     genomecol = 2,
     annotcol = c(9, 10, 19)
   ) {
@@ -29,14 +30,53 @@ distill <- function(
   annotation_table <- as.data.frame(annotation_table)
   giftdb <- as.data.frame(giftdb)
 
-  # List genomes
   if (!missing(genomecol)) {
     genomes <- unique(annotation_table[, genomecol])
   } else {
     genomes <- "GIFT"
   }
 
-  # Calculate GIFTs for each genome iteratively
+  gift_table <- compose_gift_table(genomes, annotation_table, genomecol, annotcol, giftdb)
+
+  rownames(gift_table) <- genomes
+  colnames(gift_table) <- giftdb$Code_bundle
+  gift_table[is.na(gift_table)] <- 0
+
+  return(gift_table)
+}
+
+
+compose_identifier_vector <- function(annotations_genome, annotcol) {
+  identifier_vector <-
+    annotations_genome[, annotcol] %>%  # annotcol is a number, not a name
+    c() %>%
+    unlist() %>%
+    c() %>%
+    stringr::str_extract(  # Parse identifiers (KEGG|EC)
+      pattern = "K[0-9]{5}|(?<=\\[EC:).+?(?=\\])"
+    ) %>%
+    unique() %>% # Dereplicate
+    stats::na.exclude() %>%
+    stringr::str_split(pattern = " ") %>%
+    unlist() %>%
+    .[!grepl(pattern = "-", x = ., fixed = TRUE)] # Remove ambiguous EC codes  # nolint
+  return(identifier_vector)
+}
+
+
+compose_gift_vector <- function(identifier_vector, giftdb=distillR::GIFT_db) {
+  gift_list <- list()
+  for (row_id in seq_len(nrow(giftdb))) {
+    definition <- giftdb[row_id, "Definition"]
+    gift_list[[row_id]] <- compute_gift(definition, identifier_vector)
+  }
+  gift_vector <- as.numeric(gift_list)
+
+  return(gift_vector)
+}
+
+
+compose_gift_table <- function(genomes, annotation_table, genomecol, annotcol, giftdb) {
   gift_table <- c()
   m <- 0
   for (genome in genomes) {
@@ -45,42 +85,14 @@ distill <- function(
       annotations_genome <-
         annotation_table[annotation_table[, genomecol] == genome, ]
     } else {
-      annotations_genome <-
-        annotation_table
+      annotations_genome <- annotation_table
     }
 
-    # Create vector of identifiers
-    identifier_vector <-
-      annotations_genome[, annotcol] %>%
-      c() %>%
-      unlist() %>%
-      c() %>%
-      stringr::str_extract(  # Parse identifiers (KEGG|EC)
-        pattern = "K[0-9]{5}|(?<=\\[EC:).+?(?=\\])"
-      ) %>%
-      unique() %>% # Dereplicate
-      stats::na.exclude() %>%
-      stringr::str_split(pattern = " ") %>%
-      unlist() %>%
-      .[!grepl(pattern = "-", x = ., fixed = TRUE)] # Remove ambiguous EC codes  # nolint
+    identifier_vector <- compose_identifier_vector(annotations_genome, annotcol)
 
-    # Calculate GIFTs for each Pathway and append to vector
-    gift_vector <- c()
-    for (row_id in seq_len(nrow(giftdb))) {
-      definition <- giftdb[row_id, "Definition"]
-      gift <- compute_gift(definition, identifier_vector)
-      gift_vector <- c(gift_vector, gift)
-    }
-    # Append GIFT vector of the genome to the GIFT table containing GIFT values
-    # of all genomes
+    gift_vector <- compose_gift_vector(identifier_vector, giftdb)
     gift_table <- rbind(gift_table, gift_vector)
   }
 
-  # Format output GIFT table
-  rownames(gift_table) <- genomes
-  colnames(gift_table) <- giftdb$Code_bundle
-  gift_table[is.na(gift_table)] <- 0
-
-  # Output GIFT table
   return(gift_table)
 }
