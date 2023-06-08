5af76c72a3ff5a617be99bacda68dbbfb2f1040a
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-06-08 17:19:52 +0200

    load / save test

--- tests/testthat/test-create_step_matrix.R
+++ tests/testthat/test-create_step_matrix.R
@@ -4,6 +4,7 @@ test_that("Test create_step_matrix", {
   definition_levels <-
     readr::read_rds(test_path("fixtures", "definition_levels.rds"))
   actual <- create_step_matrix(definition_decomposed, definition_levels)
+  # readr::write_rds(actual, test_path("fixtures", "step_matrix.rds"))  # nolint
   expected <- readr::read_rds(test_path("fixtures", "step_matrix.rds"))
   expect_equal(actual, expected)
 })
