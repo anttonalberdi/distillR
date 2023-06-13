8df75badfc35a91de88e5b1af0ada53500dfb63d
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-06-13 10:13:04 +0200

    Comment distillq tests

--- tests/testthat/test-distill_definition.R
+++ tests/testthat/test-distill_definition.R
@@ -11,41 +11,41 @@ test_that("Test distill_definition on L2_group", {
   expect_equal(actual, expected)
 })
 
-test_that("Test process_cluster L0", {
-  # expect_true(TRUE)
-})
+# test_that("Test process_cluster L0", {
+# expect_true(TRUE)
+# })
 
-test_that("Test process_cluster L1", {
-  # expect_true(TRUE)
-})
+# test_that("Test process_cluster L1", {
+# expect_true(TRUE)
+# })
 
-test_that("Test process_cluster other levels", {
-  # expect_true(TRUE)
-})
+# test_that("Test process_cluster other levels", {
+# expect_true(TRUE)
+# })
 
-test_that("Test process_subdef2 on link", {
-  # subdef <-
-  # subdef2 <- subdef[(subdef != " ") & (subdef != "+")]
-  # present <-
-  # actual <- process_subdef2(subdef2, present)
-  # expected <-
-  # expect_true(TRUE)
-})
+# test_that("Test process_subdef2 on link", {
+# subdef <-
+# subdef2 <- subdef[(subdef != " ") & (subdef != "+")]
+# present <-
+# actual <- process_subdef2(subdef2, present)
+# expected <-
+# expect_true(TRUE)
+# })
 
 
-test_that("Test get_value space or plus", {
-  # subdef <-
-  # present <-
-  # actual <-
-  # expected <-
-  # expect_equal(actual, expected)
-})
+# test_that("Test get_value space or plus", {
+# subdef <-
+# present <-
+# actual <-
+# expected <-
+# expect_equal(actual, expected)
+# })
 
-test_that("Test get_value comma", {
-  # subdef <-
-  # present <-
-  # actual <-
-  # expected <-
-  # expect_equal(actual, expected)
-  # expect_true(TRUE)
-})
+# test_that("Test get_value comma", {
+# subdef <-
+# present <-
+# actual <-
+# expected <-
+# expect_equal(actual, expected)
+# expect_true(TRUE)
+# })
--- tests/testthat/test-distillq_definition.R
+++ tests/testthat/test-distillq_definition.R
@@ -1,6 +1,6 @@
-test_that("Test distillq_definition", {
-  # gene_count_table <- distillR::gene_expression
-  # annotation_table <- distillR::gene_annotations
-  # GIFT_db <- distillR::GIFT_db
-  # actual <- distillq(gene_count_table, annotation_table, GIFT_db,  genecol = 1, genomecol = 2, annotcol = c(9, 10, 19))
-})
+# test_that("Test distillq_definition", {
+#   gene_count_table <- distillR::gene_expression
+#   annotation_table <- distillR::gene_annotations
+#   GIFT_db <- distillR::GIFT_db
+#   actual <- distillq(gene_count_table, annotation_table, GIFT_db,  genecol = 1, genomecol = 2, annotcol = c(9, 10, 19))
+# })
