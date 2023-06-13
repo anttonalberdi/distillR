d49ad64436596d82c713d3f505a60e65d3f918da
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-06-13 10:35:47 +0200

    Merge distillq tests

--- tests/testthat/test-distillq.R
+++ tests/testthat/test-distillq.R
@@ -1,3 +1,5 @@
+# distillq ----
+
 # nolint start
 # test_that("Test distillq", {
 #   actual <- distillq(
@@ -11,3 +13,11 @@
 #   expect_true(TRUE)
 # })
 # nolint end
+
+# distillq_definition ----
+# test_that("Test distillq_definition", {
+#   gene_count_table <- distillR::gene_expression
+#   annotation_table <- distillR::gene_annotations
+#   GIFT_db <- distillR::GIFT_db
+#   actual <- distillq(gene_count_table, annotation_table, GIFT_db,  genecol = 1, genomecol = 2, annotcol = c(9, 10, 19))
+# })
