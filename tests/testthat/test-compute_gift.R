9d165a5af6a343f633e98073b5bc4307a3fc3c29
Jorge Langa <jorge.langa.arranz@gmail.com>
2023-06-13 10:18:16 +0200

    Merge distill tests

--- tests/testthat/test-compute_gift.R
+++
@@ -1,7 +0,0 @@
-test_that("Test compute_gift", {
-  definition <- readr::read_rds(test_path("fixtures", "definition.rds"))
-  present <- readr::read_rds(test_path("fixtures", "present.rds"))
-  actual <- compute_gift(definition = definition, present = present)
-  expected <- 0.67
-  expect_equal(actual, expected)
-})
--- tests/testthat/test-set_levels.R
+++
@@ -1,9 +0,0 @@
-test_that("Check that set_levels work", {
-  definition_decomposed <- c(
-    "K01580", " ", "(", "K13524", ",", "K07250", ",", "K00823", ",", "K16871",
-    ")", " ", "(", "K00135", ",", "(", "K00139", ",", "K17761", ")", ")"
-  )
-  actual <- set_levels(definition_decomposed)
-  expected <- c(0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 2, 2, 2, 2, 1, 0)
-  expect_equal(actual, expected)
-})
