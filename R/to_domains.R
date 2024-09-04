d3498205e3e7c5fdacf75ddf62faa018a1387aef
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2024-09-04 14:17:14 +0200

    feat(to_domains): implementation + tests

---
+++ R/to_domains.R
@@ -0,0 +1,24 @@
+#' Aggregate domain-level GIFTs
+#'
+#' @param distilled tibble result from distill
+#'
+#' @return tibble with aggregated domain-level GIFTs
+#' @export
+#'
+
+to_domains <- function(distilled) {
+  distilled %>%
+    dplyr::left_join(gift_df, by = "pathway_id") %>%
+    dplyr::select(
+      mag_id, domain_id, domain_name, pathway_id, length_shortest_path, cost
+    ) %>%
+    dplyr::distinct() %>%
+    dplyr::group_by(mag_id, domain_id, domain_name) %>%
+    dplyr::summarise(
+      length_shortest_path = sum(length_shortest_path),
+      cost = sum(cost)
+    ) %>%
+    dplyr::mutate(
+      completeness = 1 - (cost / length_shortest_path),
+    )
+}
---
+++ tests/testthat/test-to_domains.R
@@ -0,0 +1,27 @@
+test_that("to_function returns something", {
+  distilled <- readr::read_rds(test_path("fixtures", "distill_expected.rds"))
+  expect_visible(to_domains(distilled))
+})
+
+
+test_that("to_function returns the correct tibble dimensions", {
+  distilled <- readr::read_rds(test_path("fixtures", "distill_expected.rds"))
+
+  actual <- to_domains(distilled)
+  actual_dims <- dim(distilled)
+  expect_equal(actual_dims, c(315, 4))
+
+})
+
+
+test_that("to_function returns the correct tibble", {
+  distilled <- readr::read_rds(test_path("fixtures", "distill_expected.rds"))
+  actual <- distilled %>% to_domains()
+
+  # readr::write_rds(x = actual, file = "tests/testthat/fixtures/to_domains_expected.rds")  # nolint
+  expected <- readr::read_rds(
+    test_path("fixtures", "to_domains_expected.rds")
+  )
+
+  expect_equal(actual, expected)
+})
