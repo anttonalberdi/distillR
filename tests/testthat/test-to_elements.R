227261680b1c70b95c13005878249d3ced57ff0c
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2024-09-04 14:38:03 +0200

    feat(to_elements): implementation + tests

---
+++ tests/testthat/test-to_elements.R
@@ -0,0 +1,27 @@
+test_that("to_function returns something", {
+  distilled <- readr::read_rds(test_path("fixtures", "distill_expected.rds"))
+  expect_visible(to_elements(distilled))
+})
+
+
+test_that("to_function returns the correct tibble dimensions", {
+  distilled <- readr::read_rds(test_path("fixtures", "distill_expected.rds"))
+
+  actual <- to_elements(distilled)
+  actual_dims <- dim(distilled)
+  expect_equal(actual_dims, c(315, 4))
+
+})
+
+
+test_that("to_function returns the correct tibble", {
+  distilled <- readr::read_rds(test_path("fixtures", "distill_expected.rds"))
+  actual <- distilled %>% to_elements()
+
+  # readr::write_rds(x = actual, file = "tests/testthat/fixtures/to_elements_expected.rds")  # nolint
+  expected <- readr::read_rds(
+    test_path("fixtures", "to_elements_expected.rds")
+  )
+
+  expect_equal(actual, expected)
+})
