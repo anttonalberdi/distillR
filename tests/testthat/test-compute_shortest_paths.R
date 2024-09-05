31fdef39f1aa4ad7e0e678f225a567401c4087d8
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2024-09-05 14:24:16 +0200

    feat(compute_shortest_paths): expand tests

--- tests/testthat/test-compute_shortest_paths.R
+++ tests/testthat/test-compute_shortest_paths.R
@@ -23,3 +23,22 @@ test_that("compute_shortest_paths returns the correct tibble data", {
   )
   expect_equal(actual, expected)
 })
+
+
+test_that("compute_shortest_paths computes as complete an entire element", {
+
+  annotations <- c(
+    "K00601", "K00602", "K00764", "K01492", "K01587", "K01588", "K01589",
+    "K01756", "K01923", "K01933", "K01945", "K01952", "K06863", "K08289",
+    "K11176", "K11787", "K11788", "K11808", "K13713", "K23264", "K23265",
+    "K23269", "K23270"
+  )
+
+  observed_cost <-
+    annotations %>%
+    compute_shortest_paths() %>%
+    dplyr::filter(pathway_id == "B010101") %>%
+    dplyr::pull(cost)
+
+  expect_equal(observed_cost, 0)
+})
