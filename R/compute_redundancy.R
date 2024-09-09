51d37aa063554cd451e3031aa80d03269f8ad8c6
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2024-09-09 16:41:07 +0200

    feat: add compute_redundancy

---
+++ R/compute_redundancy.R
@@ -0,0 +1,36 @@
+#' Compute the number of annotations present in a given pathway
+#'
+#' @param annotation_vector  A vector of annotations to compute the redundancy
+#'
+#' @return A tibble with the following columns:
+#' - `pathway_id`: The pathway identifier
+#' - `present`: The number of annotations present in the pathway
+#' - `total`: The total number of annotations in the pathway
+#' @export
+#'
+#' @examples
+#' compute_redundancy(c("S09X", "6.1.1.4"))
+compute_redundancy <- function(annotation_vector){
+
+  annotation_vector_clean <- annotation_vector %>% unique()
+
+  gift_df %>%
+    dplyr::select(pathway_id, from, to) %>%
+    tidyr::pivot_longer(-pathway_id) %>%
+    tidyr::separate(value, into = c("module", "ko", "level"), sep = "_") %>%
+    dplyr::select(pathway_id, ko) %>%
+    dplyr::distinct() %>%
+    dplyr::filter(ko != "root") %>%
+    dplyr::summarise(
+      ko_ids = list(ko),
+      .by = pathway_id
+    ) %>%
+    dplyr::rowwise() %>%
+    dplyr::mutate(
+      present = sum(annotation_vector_clean %in% ko_ids),
+      total = length(ko_ids)
+    ) %>%
+    dplyr::select(pathway_id, present, total) %>%
+    dplyr::ungroup()
+
+}
--- R/globals.R
+++ R/globals.R
@@ -6,5 +6,6 @@ utils::globalVariables(c(
   "to_code", "to_level", "Code_bundle", "from", "to", "pathway_id", "mag_id",
   "gene_id", "function_id", "function_name", "domain_id", "domain_name",
   "element_id", "element_name", "fasta", "...1", "ko_id", "cazy_ids",
-  "dram_annotations", "bundle_cost", "annotation_ids", "dram_annotation"
+  "dram_annotations", "bundle_cost", "annotation_ids", "dram_annotation",
+  "ko", "ko_ids", "present", "total", "value"
 ))
